65a1c343e82a22b11065891e249540fe1305b6e3
bbek4210 <bhandaribibek731@gmail.com>
2023-11-30 23:47:16 +0545

    b

--- app/components/basics/MenuCard.js
+++ app/components/basics/MenuCard.js
@@ -6,20 +6,22 @@ const menucard = ({menuData}) => {
     <>
 <section className="main-card--container ">
 {menuData.map((curElem) =>{
+  const {id, name,category,image,description,price} = curElem
   return(
     <>
     
-    <div className='card-container ' key={curElem.id} >
+    <div className='card-container ' key={id} >
             <div className='card '>
               <div className='card-body '>
-                <span className='card-number card-circle subtle'>{curElem.id}</span>
-                <span className='card-author subtle'> {curElem.name} </span>
-                <h2 className='card-title'>{curElem.name}</h2>
-                <span className='card-description subtle'> {curElem.description} 
+                <span className='card-number card-circle subtle'>{id}</span>
+                <span className='card-author subtle'> {name} </span>
+                <h2 className='card-title'>{name}</h2>
+                <span className='card-description subtle'> {description} 
                 </span>
                 <div className='card-read'>Read</div>
-                <img src={curElem.image}alt="" className='card-media rounded-3xl' />
+                <img src={image}alt="" className='card-media rounded-3xl' />
                 <span className='card-tag subtle'>Order Now</span>
+                <span className='card-tag subtle mx-1'>{price}</span>
 
               </div>
             </div>
--- app/components/basics/Restro.js
+++ app/components/basics/Restro.js
@@ -2,11 +2,31 @@
 import React,{ useState} from 'react'
 import Menu from "./menuApi.js";
 import MenuCard from "./MenuCard.js";
+import navbar from './navbar.js';
+
+const newList = [...new Set(Menu.map((curElem)=>{
+  return curElem.category
+  
+
+
+}))]
+
 
 const Restro = () => {
  const[menuData, setMenuData] =useState(Menu);
+
+ const filterItem =(category)=>{
+  const updatedList =Menu.filter ((curElem)=>{
+    return curElem.category===category;
+  })
+  setMenuData(updatedList)
+ }
  
   return <> 
+
+
+
+
   <MenuCard menuData={menuData} />
     </>
   
